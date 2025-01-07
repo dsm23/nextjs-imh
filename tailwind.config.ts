ea3f6c56fbbb08aa92a29b73a308571fb38f7b5b
David Murdoch <dsmurdoch357@gmail.com>
2025-01-07 23:42:23 +0000

    chore(src/): introduce src/ directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
