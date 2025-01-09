3f21d75eb143be48b86591a7095530843e0e6079
David Murdoch <dsmurdoch357@gmail.com>
2025-01-09 00:00:41 +0000

    feat(health check): add health check

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,14 @@ const config: NextConfig = {
     ],
   },
   productionBrowserSourceMaps: true,
+  async rewrites() {
+    return await [
+      { source: "/healthz", destination: "/api/health" },
+      { source: "/api/healthz", destination: "/api/health" },
+      { source: "/health", destination: "/api/health" },
+      { source: "/ping", destination: "/api/health" },
+    ];
+  },
 };
 
 export default config;
