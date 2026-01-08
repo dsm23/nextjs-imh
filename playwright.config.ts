a4fa07df726f346a995a9faa54516fe774a132b3
David Murdoch <dsmurdoch357@gmail.com>
2026-01-08 16:53:12 +0000

    chore(playwright): separate configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,26 +29,16 @@ envFiles.forEach((file) => {
  */
 export default defineConfig({
   testDir: "./playwright-tests",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: process.env.CI ? "blob" : "html",
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: `http://127.0.0.1:${PORT}`,
+    baseURL: `http://localhost:${PORT}`,
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
@@ -65,11 +55,10 @@ export default defineConfig({
       use: { ...devices["Desktop Safari"] },
     },
   ],
-
-  /* Run your local dev server before starting the tests */
+  tag: "@dev",
   webServer: {
     command: "pnpm run dev",
-    url: `http://127.0.0.1:${PORT}`,
+    url: `http://localhost:${PORT}`,
     reuseExistingServer: !process.env.CI,
   },
 });
