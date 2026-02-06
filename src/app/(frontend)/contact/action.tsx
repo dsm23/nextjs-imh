ed7892c7e8ec5d979d5985ef3118b1dcb0d6d3cf
David Murdoch <dsmurdoch357@gmail.com>
2026-02-06 11:45:44 +0000

    fix(email): fix contact form email recipients

--- src/app/(frontend)/contact/action.tsx
+++ src/app/(frontend)/contact/action.tsx
@@ -8,10 +8,8 @@ const sendEmail = async ({ name, email, message }: Values) => {
   const resend = new Resend(process.env.RESEND_API_KEY);
 
   await resend.emails.send({
-    from: "IMH site contact page <contact@resend.davidmurdoch.site>",
-    // to: ["ian@imh.co.uk", "sales@imh.co.uk"],
-    // to: ["dsmurdoch357@gmail.com"],
-    to: [],
+    from: `IMH site contact page <${process.env.NEXT_PUBLIC_CONTACT_EMAIL_FROM}>`,
+    to: process.env.NEXT_PUBLIC_CONTACT_EMAIL_TO?.split(",") ?? [],
     subject: "New contact form submission",
     react: <Email name={name} email={email} message={message} />,
   });
