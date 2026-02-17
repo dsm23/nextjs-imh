99947bb16b44fc7c8a8d37f968744a2657d57fd8
David Murdoch <dsmurdoch357@gmail.com>
2026-02-17 11:53:26 +0000

    chore(csp): add remaining directives for preview mode

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,16 +7,17 @@ export function proxy(request: NextRequest) {
   // https://docs.mapbox.com/mapbox-gl-js/guides/browsers-and-testing/#csp-directives
   const cspHeader = `
     default-src 'none';
-    script-src 'strict-dynamic' 'nonce-${nonce}' https://www.googletagmanager.com;
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://*.google-analytics.com https://*.googletagmanager.com;
-    font-src 'self';
+    script-src 'strict-dynamic' 'nonce-${nonce}' https://tagmanager.google.com https://*.googletagmanager.com;
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.g.doubleclick.net https://*.googletagmanager.com https://*.google.com;
+    img-src 'self' blob: data: https://ssl.gstatic.com https://www.gstatic.com https://*.google-analytics.com https://*.googletagmanager.com;
+    font-src 'self' data: https://fonts.gstatic.com;
     worker-src 'self' blob:;
     child-src blob:;
-    connect-src 'self' https://api.resend.com/emails https://*.tiles.mapbox.com https://api.mapbox.com https://events.mapbox.com https://www.google.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com;
+    connect-src 'self' https://api.resend.com/emails https://*.tiles.mapbox.com https://api.mapbox.com https://events.mapbox.com https://pagead2.googlesyndication.com https://*.g.doubleclick.net https://*.google-analytics.com https://*.google.com https://*.googletagmanager.com;
     manifest-src 'self';
     base-uri 'self';
     form-action 'self';
+    frame-src 'self' https://www.googletagmanager.com;
     frame-ancestors 'none';
     upgrade-insecure-requests;
 `;
